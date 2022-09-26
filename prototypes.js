3717dd1754f600d3db8f5e7e2693485376cefdb0
IradJean <jadongenda@gmail.com>
2022-09-26 20:30:09 +0200

    wordCounts function

--- prototypes.js
+++ prototypes.js
@@ -2,3 +2,11 @@
 prototype refers to a mechanism 
 used to inherit properties from other objects
 */
+// function to count for the words in a composition
+function countWords(str)
+{
+    let paragraph = prompt('Type your compposition here');
+    let splittedWords = paragraph.split(' ');
+    return splittedWords.length;
+}
+console.log(countWords('hello World!'));
