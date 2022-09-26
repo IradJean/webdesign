ee94c92432251ff1e52ecbcec8fd68c6c2645df0
IradJean <jadongenda@gmail.com>
2022-09-26 20:14:01 +0200

    adding a reversing function

--- prototypes.js
+++ prototypes.js
@@ -2,3 +2,10 @@
 prototype refers to a mechanism 
 used to inherit properties from other objects
 */
+/*
+checking GIT TRACKINGS */
+function mutateArr(arr)
+{
+    return arr.reverse();
+}
+console.log(mutateArr([2,4,6]));
